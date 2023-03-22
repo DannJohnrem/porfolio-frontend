553e4e7a8228d2bf986986384c2cbdb9d81801ad
Dann Johnrem De Guzman <jonrhem10@gmail.com>
2023-03-22 15:13:00 +0800

    hide class names for cleaner code, added assets icons for login

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,7 +5,7 @@
                 <div class="flex lg:flex-1">
                     <a href="#" class="-m-1.5 p-1.5">
                         <span class="sr-only">Your Company</span>
-                        <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        <img class="w-auto h-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                             alt="" />
                     </a>
                 </div>
@@ -14,38 +14,37 @@
                         class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
                         @click="mobileMenuOpen = true">
                         <span class="sr-only">Open main menu</span>
-                        <Bars3Icon class="h-6 w-6" aria-hidden="true" />
+                        <Bars3Icon class="w-6 h-6" aria-hidden="true" />
                     </button>
                 </div>
                 <div class="hidden lg:flex lg:gap-x-12">
                     <a v-for="item in navigation" :key="item.name" :href="item.href"
-                        class="text-sm font-semibold leading-6 text-gray-900">{{ item.name }}</a>
+                        class="text-sm font-semibold leading-6 text-gray-900 relative after:content-[''] after:bg-indigo-600 after:h-1 after:w-0 after:left-0 after:-bottom-1 after:rounded-xl after:absolute after:transition-all after:duration-300 hover:after:w-full">{{ item.name }}</a>
                 </div>
-                <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-                    <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Log in <span
-                            aria-hidden="true">&rarr;</span></a>
+                <div class="hidden lg:flex lg:flex-1 lg:justify-end lg:flex-nowrap">
+                    <a href="#" class="flex mr-1 text-sm font-semibold leading-6 text-gray-900 flex-nowrap">Log in &nbsp; <img src="../assets/icons/161-trending-flat-outline.gif" alt="" width="20"></a>
                 </div>
             </nav>
             <Dialog as="div" class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
                 <div class="fixed inset-0 z-50" />
                 <DialogPanel
-                    class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                    class="fixed inset-y-0 right-0 z-50 w-full px-6 py-6 overflow-y-auto bg-white sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div class="flex items-center justify-between">
                         <a href="#" class="-m-1.5 p-1.5">
                             <span class="sr-only">Your Company</span>
-                            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                            <img class="w-auto h-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                                 alt="" />
                         </a>
                         <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
                             <span class="sr-only">Close menu</span>
-                            <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+                            <XMarkIcon class="w-6 h-6" aria-hidden="true" />
                         </button>
                     </div>
-                    <div class="mt-6 flow-root">
+                    <div class="flow-root mt-6">
                         <div class="-my-6 divide-y divide-gray-500/10">
-                            <div class="space-y-2 py-6">
+                            <div class="py-6 space-y-2">
                                 <a v-for="item in navigation" :key="item.name" :href="item.href"
-                                    class="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">{{
+                                    class="block px-3 py-2 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg hover:bg-gray-50">{{
                                         item.name }}</a>
                             </div>
                             <div class="py-6">
@@ -59,8 +58,8 @@
             </Dialog>
         </header>
 
-        <div class="relative isolate px-6 pt-14 lg:px-8">
-            <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
+        <div class="relative px-6 isolate pt-14 lg:px-8">
+            <div class="absolute inset-x-0 overflow-hidden -top-40 -z-10 transform-gpu blur-3xl sm:-top-80">
                 <svg class="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
                     viewBox="0 0 1155 678">
                     <path fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)" fill-opacity=".3"
@@ -74,10 +73,10 @@
                     </defs>
                 </svg>
             </div>
-            <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+            <div class="max-w-2xl py-32 mx-auto sm:py-48 lg:py-56">
                 <div class="hidden sm:mb-8 sm:flex sm:justify-center">
                     <div
-                        class="relative rounded-full py-1 px-3 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                        class="relative px-3 py-1 text-sm leading-6 text-gray-600 rounded-full ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                         Announcing our next round of funding.
                         <a href="#" class="font-semibold text-indigo-600"><span class="absolute inset-0"
                                 aria-hidden="true" />Read more
@@ -93,7 +92,7 @@
                         lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
                         fugiat aliqua.
                     </p>
-                    <div class="mt-10 flex items-center justify-center gap-x-6">
+                    <div class="flex items-center justify-center mt-10 gap-x-6">
                         <a href="#"
                             class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get
                             started</a>
@@ -127,10 +126,11 @@ import { Dialog, DialogPanel } from "@headlessui/vue";
 import { Bars3Icon, XMarkIcon } from "@heroicons/vue/24/outline";
 
 const navigation = [
-    { name: "Product", href: "#" },
-    { name: "Features", href: "#" },
-    { name: "Marketplace", href: "#" },
-    { name: "Company", href: "#" },
+    { name: "Home", href: "#" },
+    { name: "About", href: "#" },
+    { name: "Services", href: "#" },
+    { name: "Portfolio", href: "#" },
+    { name: "Contact", href: "#" },
 ];
 
 const mobileMenuOpen = ref(false);
